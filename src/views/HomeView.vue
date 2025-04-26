cde4c1ceae6c4316746045d43a457db72d75bef0
revalisa@revalisa <revalisa@revalisa>
2025-04-26 12:38:50 +0700

    home data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
 
-</script>
+import { ref, onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 
+interface Student {
+  id          : number
+  namaLengkap : string
+  tempatLahir : string
+  tanggalLahir: number
+  jenisKelamin: string
+  alamat      : string
+  noTelepon   : number
+  asalSekolah : string
+}
+
+const students = ref<Student[]>([])
+
+const fetchStudents = async () => {
+  const response = await fetch('/api/addStudent')
+  const data = await response.json()
+  students.value = data
+}
+
+onMounted(() => {
+  fetchStudents()
+})
+
+</script>
 <template>
-  <div class="home">
-    <h1>This is a home page</h1>
-  </div>
+  <main>
+    <div>Data Murid</div>
+    <div>
+      <RouterLink to="/student">Daftar</RouterLink>
+    </div>
+    <div>
+      <ul>
+        <li v-for="student in students" :key="student.id">
+          <div>{{ student.namaLengkap }}</div>
+          <div>{{ student.tempatLahir }}</div>
+          <div>{{ new Date(Number(student.tanggalLahir) * 1000).toLocaleString() }}</div>
+          <div>{{ student.jenisKelamin }}</div>
+          <div>{{ student.alamat }}</div>
+          <div>{{ student.noTelepon }}</div>
+          <div>{{ student.asalSekolah }}</div>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
