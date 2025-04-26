46c360c858592b40d2e48f6cea24a8b390542590
revalisa@revalisa <revalisa@revalisa>
2025-04-26 13:03:33 +0700

    router untuk addStudent

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/student',
+      name: 'addStudent',
+      component: () => import('../views/AddStudentView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
