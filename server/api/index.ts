aad057ca551900fd7fb1692ad9dbb9324a56bb41
revalisa@revalisa <revalisa@revalisa>
2025-05-22 15:05:10 +0700

    menggunakan hono untuk backend

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,24 +1,23 @@
-interface Env {
-    ASSETS: Fetcher;
+import { Hono } from 'hono';
+
+type Bindings = {
     DB: D1Database;
-}
-export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
+    ASSETS: Fetcher;
+};
 
-     if (url.pathname.startsWith("/api/students")) {
-        if (request.method == 'GET'){
-            let { results } = await env.DB.prepare('SELECT * FROM students').all();
-            return Response.json(results);
-        } else if (request.method == 'POST') {
-            const newId = crypto.randomUUID()
-            const input = await request.json<any>()
-            const query = `INSERT INTO students (namaLengkap,tempatLahir,tanggalLahir,jenisKelamin,alamat,noTelepon,asalSekolah) values ("${input.namaLengkap}","${input.tempatLahir}",${input.tanggalLahir},"${input.jenisKelamin}","${input.alamat}","${input.noTelepon}","${input.asalSekolah}")`;
-            const newStudent= await env. DB.exec(query);
-            return Response.json(newStudent); 
-        }
-     } 
+const app = new Hono<{ Bindings: Bindings}>()
 
-     return env.ASSETS.fetch(request);
-    },
-} satisfies ExportedHandler<Env>;
+app.get('/api/students', async (c) => {
+    let { results } = await c.env.DB.prepare('SELECT * FROM students').all();
+    return c.json(results); 
+})
+
+app.post('/api/students', async (c) => {
+    const newId = crypto.randomUUID()
+    const input = await c.req.json<any>()
+    const query = `INSERT INTO students (namaLengkap,tempatLahir,tanggalLahir,jenisKelamin,alamat,noTelepon,asalSekolah) values ("${input.namaLengkap}","${input.tempatLahir}","${input.tanggalLahir}","${input.jenisKelamin}","${input.alamat}","${input.noTelepon}","${input.asalSekolah}")`;
+    const newStudent= await c.env.DB.exec(query);
+    return c.json(newStudent);
+})
+
+export default app;
