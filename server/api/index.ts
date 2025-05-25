4e75911c36bbf60e9cf5612fa1e7f202e93dc667
revalisa@revalisa <revalisa@revalisa>
2025-05-25 23:29:13 +0700

    update dan delete data

--- server/api/index.ts
+++ server/api/index.ts
@@ -15,9 +15,36 @@ app.get('/api/students', async (c) => {
 app.post('/api/students', async (c) => {
     const newId = crypto.randomUUID()
     const input = await c.req.json<any>()
-    const query = `INSERT INTO students (namaLengkap,tempatLahir,tanggalLahir,jenisKelamin,alamat,noTelepon,asalSekolah) values ("${input.namaLengkap}","${input.tempatLahir}","${input.tanggalLahir}","${input.jenisKelamin}","${input.alamat}","${input.noTelepon}","${input.asalSekolah}")`;
-    const newStudent= await c.env.DB.exec(query);
-    return c.json(newStudent);
+    const query = `INSERT INTO students (namaLengkap,tempatLahir,tanggalLahir,jenisKelamin,alamat,noTelepon,asalSekolah) values ("${input.namaLengkap}","${input.tempatLahir}","${input.tanggalLahir}","${input.jenisKelamin}","${input.alamat}","${input.noTelepon}","${input.asalSekolah}")`
+    const newStudent= await c.env.DB.exec(query)
+    return c.json(newStudent)
 })
+
+app.get('/api/students/:id', async (c) => {
+    const studentId = c.req.param('id')
+    let { results } = await c.env.DB.prepare('SELECT * FROM students WHERE id = ?').bind(studentId).all()
+    return c.json(results[0])
+})
+
+app.put('api/students/:id', async (c) => {
+    const studentId = c.req.param('id')
+
+    const input = await c.req.json<any>()
+    const query = 'UPDATE students SET namaLengkap = "${input.namaLengkap}", tempatLahir = "${input.tempatLahir}", jenisKelamin = "${input.jenisKelamin}", alamat = "${input.alamat}", noTelepon = "${input.noTelepon}", asalSekolah = "${input.asalSekolah}" WHERE id = "${studentId}}"'
+    const student = await c.env.DB.exec(query)
+
+    return c.json(student)
+})
+
+app.delete('api/students/:id', async (c) => {
+    const studentId = c.req.param('id')
+
+    const query = 'DELETE FROM students WHERE id = "${studentId}"'
+    const student = await c.env.DB.exec(query)
+
+    return c.json(student)
+    })
+
+
 
 export default app;
