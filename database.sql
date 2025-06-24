65760fae76d2e2a8b1c8756d3aef310a99026012
revalisa@revalisa <155284969+revalisa@users.noreply.github.com>
2025-06-25 00:10:08 +0700

    database new

--- database.sql
+++ database.sql
@@ -1,14 +1,36 @@
--- Buat tabel events
-CREATE TABLE IF NOT EXISTS events (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- Hapus tabel events jika ada
+DROP TABLE IF EXISTS events;
+
+-- -- -- Buat ulang tabel events
+CREATE TABLE events (
+  id INTEGER PRIMARY KEY,
   judul TEXT NOT NULL,
   isi TEXT NOT NULL,
-  tanggal TEXT NOT NULL DEFAULT CURRENT_DATE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  tanggal DATE
 );
 
--- Sisipkan data awal dengan menyebutkan kolom yang diisi
-INSERT INTO events (judul, isi, tanggal) VALUES
-  ('Pembukaan PPDB Online 2025', 'Pendaftaran dibuka mulai 1 Juni hingga 15 Juni 2025 secara online.', '2025-06-01'),
-  ('Pengumuman Hasil Seleksi Tahap 1', 'Hasil seleksi tahap pertama akan diumumkan pada 18 Juni 2025.', '2025-06-18'),
-  ('Daftar Ulang Tahap 1', 'Peserta yang lolos wajib daftar ulang pada 19-21 Juni 2025.', '2025-06-19');
+INSERT INTO events (judul,isi,tanggal) 
+VALUES
+  ( 'Pembukaan PPDB Online 2025', 'Pendaftaran dibuka mulai 1 Juni hingga 15 Juni 2025 secara online.', '2025-06-01'),
+  ('Pengumuman Hasil Seleksi Tahap 1', 'Hasil seleksi tahap pertama akan diumumkan pada 18 Juni 2025.', '2025-06-18');
+
+-- -- -- Hapus tabel students jika ada
+-- Drop tabel jika sudah ada
+-- DROP TABLE IF EXISTS students;
+
+-- -- Buat ulang tabel students
+-- CREATE TABLE students (
+--   id INTEGER PRIMARY KEY,
+--   namaLengkap TEXT NOT NULL,
+--   tempatLahir TEXT,
+--   tanggalLahir DATE,
+--   jenisKelamin TEXT,
+--   alamat TEXT,
+--   noTelepon TEXT,
+--   asalSekolah TEXT
+-- );
+
+-- -- Menyisipkan data ke dalam tabel students
+-- INSERT INTO students (id, namaLengkap, tempatLahir, tanggalLahir, jenisKelamin, alamat, noTelepon, asalSekolah) 
+-- VALUES 
+--   (1, 'Randi Satya', 'Malang', '2004-05-10', 'L', 'Malang', '081334138379', 'SMK 1 Malang');
